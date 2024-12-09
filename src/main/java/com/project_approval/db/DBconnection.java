93f1b8e2c6154cc1447d0bcb61eba2d4f1e77919
unknown <akashvyawhare73@gmail.com>
2024-12-09 22:50:52 +0530

    db class & packages

--- src/main/java/com/project_approval/db/DBconnection.java
+++ src/main/java/com/project_approval/db/DBconnection.java
@@ -1,5 +1,25 @@
 package com.project_approval.db;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class DBconnection {
 
+	private static Connection connection;
+
+	public static Connection getDbConnection() {
+
+		if (connection == null) {
+
+			try {
+				Class.forName("com.mysql.cj.jdbc.Driver");
+				connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/project_approval", "root", "root");
+			} catch (Exception e) {
+				
+				e.printStackTrace();
+			}
+		}
+
+		return connection;
+	}
 }
